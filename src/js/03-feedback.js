54063ecabe7e6ae1fe1b0422730ecfe4d3dac396
fedorzvyk <fedorzvyk@gmail.com>
2022-10-28 20:58:28 +0300

    add task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+const formRef = document.querySelector('.feedback-form');
+const formData = {};
+
+formRef.addEventListener('submit', onFormSubmit);
+formRef.addEventListener('input', throttle(onFormInput, 500));
+
+populateTextarea();
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  if (formRef[0].value && formRef[1].value) {
+    console.log(formData);
+  }
+
+  evt.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function onFormInput(evt) {
+  formData[evt.target.name] = evt.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function populateTextarea() {
+  const savedMessage = localStorage.getItem(STORAGE_KEY);
+  const parsedMessage = JSON.parse(savedMessage);
+  if (savedMessage) {
+    formRef[0].value = parsedMessage.email;
+    formRef[1].value = parsedMessage.message;
+    formData[formRef[0].name] = formRef[0].value;
+    formData[formRef[1].name] = formRef[1].value;
+  }
+}
